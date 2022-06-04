ce2fa7422055eb69f0a88e80e06e501412a1ec0e
zhiyuemeng <99643511+zhiyuemeng@users.noreply.github.com>
2022-06-04 23:01:13 +0800

    pull commit

--- src/main/java/test/GitTest.java
+++ src/main/java/test/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello, master!");
         System.out.println("hello, got-fix!");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
