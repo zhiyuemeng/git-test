b926a94e8c9b83c63905e255000be5d0908d2ab1
zhizhi <zhizhi@123.com>
2022-06-04 22:58:19 +0800

    push commit

--- src/main/java/test/GitTest.java
+++ src/main/java/test/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello, git4!");
         System.out.println("hello, master!");
         System.out.println("hello, got-fix!");
+        System.out.println("push test");
     }
 }
